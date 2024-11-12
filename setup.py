75413dd87a39fec9d5690acb314fab9358f2b3b1
valera2444 <valerkaf2003.vf@gmail.com>
2024-11-12 22:16:04 +0300

    DS-4 Changes: - Github action on push added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="predict-future-sales",
-    version="1.0",
+    version="1.1",
     author="valera2444",
     author_email="valerkaf2003.vf@gmail.com",
     description="Feature extraction, validation schema and hyperparameters tuning for future sales prediction",
